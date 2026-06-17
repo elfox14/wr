a2ec94a6e7d5765489e1c4d09c43a7bd5a1f6e3c
elfox14 <86858355+elfox14@users.noreply.github.com>
2026-06-17 04:59:07 +0300

    Add animation player barrel exports

---
+++ app/animation-live/player/index.ts
@@ -0,0 +1,31 @@
+export type {
+  Team,
+  Snapshot,
+  EventSide,
+  PressureSide,
+  EventFilterKey,
+  EventCategory,
+  MatchEvent,
+  LiveStatsResponse,
+  LiveEventsResponse,
+  PressureWindow,
+  PressureModel,
+  MomentumDefinition,
+  MomentumSegment,
+  MomentumAccum,
+  DataQuality,
+} from './types';
+
+export { default as TeamName } from './components/TeamName';
+export { MiniStat, StatRow } from './components/StatCards';
+export { default as IntelligenceTile } from './components/IntelligenceTile';
+export { default as MomentumCard } from './components/MomentumCard';
+
+export { ar, formatMatchDate, formatUpdatedAt, sourceLabel } from './formatters';
+export { normalizeStatus, isFinishedStatus, isHalfTimeStatus, displayMatchStatus } from './statusUtils';
+export { eventCategory, eventMatchesFilter, eventIcon, eventLabel, cleanEventDetail, eventMinute, eventSide, sortEventsByMinute } from './eventUtils';
+export { pressureLeader, sideName } from './pressureUtils';
+export { pressureWindow, liveReadout, calculatePressureModel, windowLabel } from './livePressureUtils';
+export { MOMENTUM_SEGMENTS, pressureEventWeight, momentumRating, calculateMomentumSegments, strongestMomentumSegment } from './momentumUtils';
+export { bounded, timelineLeft, stableOffset, ballPosition } from './pitchUtils';
+export { DATA_QUALITY_STAT_KEYS, n, hasAnyStat, resolvedSnapshot, matchStoryLines, dataQuality } from './matchAnalysisUtils';
