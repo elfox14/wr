ea8828e5818b3c65ff87d475f6aefb4843c27481
elfox14 <86858355+elfox14@users.noreply.github.com>
2026-06-24 02:24:26 +0300

    1

--- app/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'محفظتي | MC PRIME Exchange',
-};
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return children;
-}
